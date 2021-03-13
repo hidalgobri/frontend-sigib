316c98652970fff7717b95159ed17a91b43fd142
Cristhian Jumbo <50051312+crisjc6@users.noreply.github.com>
2021-03-13 02:06:29 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 export const environment = {
   "production": false,
-  "url": "http://lbappsigib-1924031910.us-east-1.elb.amazonaws.com",
+  "url": "http://loadb-app-335908590.us-east-1.elb.amazonaws.com",
   "port": ""
 };

04f47c4ebe9691563e96311d927793bacf25ea2f
hidalgobri <60413558+hidalgobri@users.noreply.github.com>
2021-03-16 00:48:43 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 export const environment = {
   "production": false,
-  "url": "http://loadb-app-335908590.us-east-1.elb.amazonaws.com",
+  "url": "http://loadb-app-1882559026.us-east-1.elb.amazonaws.com/",
   "port": ""
 };
